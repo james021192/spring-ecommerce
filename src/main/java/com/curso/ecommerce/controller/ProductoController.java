4341fae5ce509b8f6d18cac54000f67c90adfe16
JAMES <jjhuaman02@gmail.com>
2023-08-22 10:45:18 -0500

    impl de carga de imagen

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -1,5 +1,6 @@
 package com.curso.ecommerce.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.*;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.model.Usuario;
 import com.curso.ecommerce.service.ProductoService;
+import com.curso.ecommerce.service.UploadFileService;
 
 @Controller
 @RequestMapping("/productos")
@@ -23,6 +27,8 @@ public class ProductoController {
 	
 	@Autowired
 	private ProductoService productoService;
+	@Autowired
+	private UploadFileService upload;
 	
 	@GetMapping("")
 	public String show(Model model) {
@@ -36,11 +42,27 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/save")
-	public String save(Producto producto) {
+	public String save(Producto producto,@RequestParam("img") MultipartFile file) throws IOException {
 	 LOGGER.info("ESTE ES EL OBJ PRODUCTO {}",producto);
 	 Usuario u=new Usuario(1, "", "", "","", "", "", "");
 	 producto.setUsuario(u);
+	 //imagen
+	 if(producto.getId()==null) {//cuando se crea un producto
+		 String nombreImagen=upload.saveImage(file);
+		 producto.setImagen(nombreImagen);
+	 }else {
+		 if(file.isEmpty()) {//no tiene nada cargar la misma imagen / editamos pero no cambiamos img
+			 Producto p= new Producto();
+			p=productoService.get(producto.getId()).get();
+				producto.setImagen(p.getImagen());	
+		 }else {
+			 String nombreImagen=upload.saveImage(file);
+			 producto.setImagen(nombreImagen);
+		 }
+	 }
+	 
 	 productoService.save(producto);
+	 
 		return "redirect:/productos";
 	}
 	
--- src/main/java/com/curso/ecommerce/service/UploadFileService.java
+++ src/main/java/com/curso/ecommerce/service/UploadFileService.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.aspectj.weaver.loadtime.Options;
+ 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
