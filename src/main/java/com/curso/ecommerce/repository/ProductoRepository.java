88a4f6631d3105ca47339bcfb998317b38747916
JAMES <jjhuaman02@gmail.com>
2023-08-21 23:43:40 -0500

    2

--- src/main/java/com/curso/ecommerce/repository/ProductoRepository.java
+++ src/main/java/com/curso/ecommerce/repository/ProductoRepository.java
@@ -1,5 +1,11 @@
 package com.curso.ecommerce.repository;
 
-public interface ProductoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.curso.ecommerce.model.Producto;
+
+@Repository
+public interface ProductoRepository  extends JpaRepository<Producto, Integer >{
 
 }
