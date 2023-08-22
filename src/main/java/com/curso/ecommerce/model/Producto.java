928087f567f0f4ed0a024138006388796bdb8e28
JAMES <jjhuaman02@gmail.com>
2023-08-21 23:09:02 -0500

    primer guardado

--- src/main/java/com/curso/ecommerce/model/Producto.java
+++ src/main/java/com/curso/ecommerce/model/Producto.java
@@ -1,6 +1,17 @@
 package com.curso.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="productos")
 public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String descripcion;
@@ -8,11 +19,17 @@ public class Producto {
 	private double precio;
 	private int cantidad;
 	
+	@ManyToOne
+	private Usuario usuario;
+	
 	public Producto() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad) {
+
+
+	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad,
+			Usuario usuario) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
@@ -20,7 +37,10 @@ public class Producto {
 		this.imagen = imagen;
 		this.precio = precio;
 		this.cantidad = cantidad;
+		this.usuario = usuario;
 	}
+
+
 
 	public Integer getId() {
 		return id;
@@ -58,12 +78,21 @@ public class Producto {
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
 	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
 	@Override
 	public String toString() {
 		return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", imagen=" + imagen
 				+ ", precio=" + precio + ", cantidad=" + cantidad + "]";
 	}
+
+
 	
 
 }
