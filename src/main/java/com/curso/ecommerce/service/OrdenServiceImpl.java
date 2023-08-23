8e350dec912d1e0d0d342e8dc4a5a1106c65df5d
JAMES <jjhuaman02@gmail.com>
2023-08-23 10:24:28 -0500

    generar codigo de orden

--- src/main/java/com/curso/ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/com/curso/ecommerce/service/OrdenServiceImpl.java
@@ -1,5 +1,8 @@
 package com.curso.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,39 @@ public class OrdenServiceImpl  implements IOrdenService{
 	public Orden save(Orden orden) {
 	 
 		return ordenRepository.save(orden);
+	}
+
+	@Override
+	public List<Orden> findAll() {
+		 
+		return  ordenRepository.findAll();
+	}
+	
+	//0000010
+	public String generarNumeroOrden() {
+		int numero=0;
+		String numeroConcatenado="";
+		List<Orden> ordenes=findAll();
+		List<Integer> numeros=new ArrayList<Integer>();
+		//generar codigos para el orden
+		ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+		if (ordenes.isEmpty()) {
+			numero=1;
+		}else {
+			//obtener el numero mayor
+			numero=numeros.stream().max(Integer::compare).get();
+			numero++;
+		}
+		if (numero<10) {//0000000007
+			numeroConcatenado="000000000"+String.valueOf(numero);
+		}else if(numero<100){
+			numeroConcatenado="00000000"+String.valueOf(numero);
+		}else if(numero<1000){
+			numeroConcatenado="0000000"+String.valueOf(numero);
+		}else if(numero<10000){
+			numeroConcatenado="0000000"+String.valueOf(numero);
+		}
+		return numeroConcatenado;
 	}
 
 }
